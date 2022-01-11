2aa7ba127280891b033d1ad1f1f96b6ebdd5e394
wendellmoraisz <wendellmorais3319@gmail.com>
2022-01-10 23:04:52 -0300

    Primeiro commit

---
+++ calculator.js
@@ -0,0 +1,40 @@
+const res = document.getElementById('res')
+
+function display(id){
+    res.innerHTML += id.value
+}
+
+function clean() {
+    res.innerHTML = ''
+}
+
+function expo(){
+    res.innerHTML = calcular() ** 2
+}
+
+function raiz(){
+    res.innerHTML = Math.sqrt(calcular())
+}
+
+function dividirPorUm(){
+    res.innerHTML = 1 / calcular()
+}
+
+function calcular() {
+    res.innerHTML = eval(document.getElementById('res').innerHTML.replace('÷','/').replace('x','*').replace(',','.'))
+    return eval(document.getElementById('res').innerHTML.replace('÷','/').replace('x','*').replace(',','.'))
+}
+
+function deletar(){
+    res.innerHTML = document.getElementById('res').innerHTML.slice(0, -1)
+}
+
+function invert(){
+    if (document.getElementById('res').innerHTML[0] != '-'){
+        document.getElementById('res').innerHTML = '-' + document.getElementById('res').innerHTML 
+    } else {
+        let resultado = document.getElementById('res').innerHTML
+        resultado = resultado.replace('-', '')
+        res.innerHTML = resultado
+    }
+}
