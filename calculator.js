9a774c6326474ed69c4e8e0a09b89959f36a32d9
Wendell <wendellmorais3319@gmail.com>
2022-03-26 18:38:45 -0300

    fix

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 const res = document.getElementById('res')
 
-function display(id){
+function display(id) {
     res.innerHTML.length < 13 ? res.innerHTML += id.value : null
 }
 
@@ -8,30 +8,30 @@ function clean() {
     res.innerHTML = ''
 }
 
-function expo(){
+function expo() {
     res.innerHTML = calcular() ** 2
 }
 
-function raiz(){
+function raiz() {
     res.innerHTML = Math.sqrt(calcular())
 }
 
-function dividirPorUm(){
+function dividirPorUm() {
     res.innerHTML = 1 / calcular()
 }
 
 function calcular() {
-    res.innerHTML = eval(document.getElementById('res').innerHTML.replace('÷','/').replace('x','*').replace(',','.'))
-    return eval(document.getElementById('res').innerHTML.replace('÷','/').replace('x','*').replace(',','.'))
+    res.innerHTML = eval(document.getElementById('res').innerHTML.replace('÷', '/').replace('x', '*').replace(',', '.'))
+    return eval(document.getElementById('res').innerHTML.replace('÷', '/').replace('x', '*').replace(',', '.'))
 }
 
-function deletar(){
+function deletar() {
     res.innerHTML = document.getElementById('res').innerHTML.slice(0, -1)
 }
 
-function invert(){
-    if (document.getElementById('res').innerHTML[0] != '-'){
-        document.getElementById('res').innerHTML = '-' + document.getElementById('res').innerHTML 
+function invert() {
+    if (document.getElementById('res').innerHTML[0] != '-') {
+        document.getElementById('res').innerHTML = '-' + document.getElementById('res').innerHTML
     } else {
         let resultado = document.getElementById('res').innerHTML
         resultado = resultado.replace('-', '')
